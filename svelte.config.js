432766279b4112963975e783e741e1911826fa7f
michivo <michivo@gmail.com>
2023-02-08 15:54:51 +0100

    try firebase with functions

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "sveltekit-adapter-firebase";
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
+export default {
 	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({
+			outDir: 'build',
+			functionName: 'handler',
+			nodeVersion: '16'
+		  }),
+		},
 };
-
-export default config;
