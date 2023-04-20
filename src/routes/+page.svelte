f1de8a76ec56d972da27d48173c115a6d81d44ab
michivo <michivo@gmail.com>
2023-04-20 09:11:45 +0200

    remove obsolete code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,11 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { getGameDetails } from '../services/adaptorex';
-	import { gameData } from '../stores/gamestore';
-	import { Button, Navbar, NavbarBrand, Spinner } from 'sveltestrap';
-
-	onMount(async () => {
-		const game = await getGameDetails();
-		gameData.set(game);
-	});
+  import { Button, Navbar, NavbarBrand } from 'sveltestrap';
 </script>
 
 <Navbar color="light"><NavbarBrand>Willkommen bei putsch</NavbarBrand></Navbar>
-{#if $gameData === null}
-	<Spinner />
-{:else}
-	<div class="d-flex flex-column">
-		<Button class="btn btn-lg m-2" href="/phase1">Phase 1</Button>
-		<Button class="btn btn-lg m-2" href="/phase2">Phase 2</Button>
-		<Button class="btn btn-lg m-2" href="/phase3">Phase 3</Button>
-		<!-- <Button class="btn btn-lg m-2" href="/players">Trigger</Button> -->
-	</div>
-{/if}
+<div class="d-flex flex-column">
+  <Button class="btn btn-lg m-2" href="/phase1">Phase 1</Button>
+  <Button class="btn btn-lg m-2" href="/phase2">Phase 2</Button>
+  <Button class="btn btn-lg m-2" href="/phase3">Phase 3</Button>
+  <!-- <Button class="btn btn-lg m-2" href="/players">Trigger</Button> -->
+</div>
